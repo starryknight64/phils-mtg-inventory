16e37fb2b38f119d06b4888709ab6317cf9644a0
Phillip <pdponzer@gmail.com>
2014-03-23 22:46:24 -0500

    Added jquery and missing CSS/JavaScript from last commit

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -1,9 +1,34 @@
-if (typeof jQuery !== 'undefined') {
-	(function($) {
-		$('#spinner').ajaxStart(function() {
-			$(this).fadeIn();
-		}).ajaxStop(function() {
-			$(this).fadeOut();
-		});
-	})(jQuery);
+function addCardToChanged( select ) {
+	var thisClass = select.children().children(":selected").attr("class");
+	if( thisClass == "to-inventory" ) {
+		$("select[name=as]").attr("disabled","");
+	} else {
+		$("select[name=as]").removeAttr("disabled");		
+	}
 }
+
+$( document ).ready(function() {
+	addCardToChanged( $("select[name=to]") );
+	
+	$(".card .card-image").click( function() {
+		$(".add-card-form").toggle();
+		return false;
+	});
+
+	$("select[name=to]").change( function() {
+		addCardToChanged( $(this) );
+	});
+	
+	$(".add-card-button").click( function() {
+		var form = $(this).closest("form");
+		var action = form.attr("action");
+		var selectTo = $("select[name=to] :selected").attr("class");
+		if( selectTo == "to-deck" ) {
+			form.attr("action", action+"Deck/addCard" );
+		} else {
+			form.attr("action", action+"Inventory/addCard" );
+		}
+		form.submit();
+	});
+});
+
